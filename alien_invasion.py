b50f2b7dbcb5677818a4ab033f54ebb02a43591a
HowellNoCap <166862664+advadvadvad@users.noreply.github.com>
2025-06-18 13:34:09 +0800

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,23 +9,22 @@ class AlienInvasion:
     def __init__(self):
         """初始化游戏并创建游戏资源"""
 
-         pygame.init()
+        pygame.init()
 
-         self.screen = pygame.display.set_mode((1200, 800))
-pygame.display.set_caption("Alien Invasion")
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
 
 
 def run_game(self):
     """开始游戏的主循环"""
+    while True:
+        # 侦听键盘和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-         while True:
-# 侦听键盘和鼠标事件
-             for event in pygame.event.get():
-                     if event.type == pygame.QUIT:
-        sys.exit()
-
-# 让最近绘制的屏幕可见
-             pygame.display.flip()
+            # 让最近绘制的屏幕可见
+            pygame.display.flip()
 
 if __name__ == '__main__':
     # 创建游戏实例并运行游戏
